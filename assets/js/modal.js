820233e6fca9e2e616c2fb0369325f1b99a1b4cb
thiles03 <thiles03@gmail.com>
2021-01-17 12:57:30 +0000

    Move descriptions

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,12 +7,15 @@ var modalVid = document.querySelector(".modalVid");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var modalImage = document.querySelector(".modalImage");
 var captionText = document.querySelector(".caption");
+var modalText = document.querySelector(".modalText");
 
 Array.from(document.querySelectorAll(".projectImg")).forEach(item => {
     item.addEventListener("click", event => {
+        var body = event.target.parentElement.querySelector(".hiddenModalText");
         modal.style.display = "block";
         modalImage.src = event.target.src;
         captionText.innerHTML = event.target.alt;
+        modalText.innerHTML = body.innerHTML;
     });
 })
 
