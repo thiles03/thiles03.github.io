9246f33939afad00c2d86764d75731f079b1a4e8
thiles03 <thiles03@gmail.com>
2021-01-22 12:35:49 +0000

    Video links

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,9 +1,6 @@
 // Get the image modal
 var modal = document.querySelector(".modal");
 
-// Get the video modal
-var modalVid = document.querySelector(".modalVid");
-
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var modalImage = document.querySelector(".modalImage");
 var captionText = document.querySelector(".caption");
@@ -19,33 +16,8 @@ Array.from(document.querySelectorAll(".projectImg")).forEach(item => {
     });
 })
 
-//Get the video and embed it inside the modal
-var modalVidContent = document.querySelector(".modalVidContent");
-var captionTextVid = document.querySelector(".captionVid");
-
-var videos = ["videos/vid0.mp4",
-    "videos/vid1.mp4",
-    "videos/vid2.mp4",
-    "videos/vid3.mp4",
-    "videos/vid4.mp4"];
-
-Array.from(document.querySelectorAll(".fa-play-circle")).forEach(item => {
-    item.addEventListener("click", event => {
-        var index = event.target.id;
-        modalVid.style.display = "block";
-        modalVidContent.src = videos[index];
-        captionTextVid.innerHTML = event.target.title;
-        console.log(index);
-    });
-})
-
 // When the user clicks on <span> (x), close the image modal
 document.querySelector(".close").addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// When the user clicks on <span> (x), close the video modal
-document.querySelector(".closeVid").addEventListener("click", () => {
-    modalVid.style.display = "none";
-    modalVidContent.src = null;
-});
